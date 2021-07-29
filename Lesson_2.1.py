f2d0a7dc87f06e86b2f35b33043012f8c8250f6a
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2021-07-29 22:55:43 +0700

    fix condition age > 18

--- Lesson_2.1.py
+++ Lesson_2.1.py
@@ -7,7 +7,7 @@ while True:
     except ValueError:
         print("Invalid value")
 
-if age >= 18 and income >= 2500:
+if age > 18 and income >= 2500:
     print("Congratulations you are enough condition to borrow money! ")
 else:
     print("Sorry! You not enough condition to borrow money")
