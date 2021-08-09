38242e7102b2bbdd560974da2f09603bae077187
tronglvIA <trongluongvania@gmail.com>
2021-08-09 17:41:27 +0700

    9/8/2021

--- Lesson_6/all_in_one.py
+++ Lesson_6/all_in_one.py
@@ -65,7 +65,17 @@ def bmi_infor(weight, height):
     if bmi > 35:
         print("EXTREMLY OBESE")
 
-#
+# 6.8 exercise
+def change_upper_lower(msg):
+    new_str = ""
+    for i in range(len(msg)):
+        if msg[i].isupper():
+            new_str += msg[i].lower()
+        elif msg[i].islower():
+            new_str += msg[i].upper()
+        else:
+            new_str += msg[i]
+    return new_str
 
 
 
