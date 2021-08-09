3cf53c0d07c4bb043e7c161ed6be3dcd47a24453
tronglvIA <trongluongvania@gmail.com>
2021-08-09 17:24:47 +0700

    9/8/2021

--- Lesson_6/all_in_one.py
+++ Lesson_6/all_in_one.py
@@ -47,7 +47,25 @@ def count_upper_lower(msg):
 def is_pangram(msg, alphabet):
     return not set(alphabet) - set(msg.lower())
 
-# 6.7
+# 6.7 App BMI
+def bmi_calculator(weight, height):
+    result_bmi = weight/(height*2)
+    return result_bmi
+
+def bmi_infor(weight, height):
+    bmi = bmi_calculator(weight, height)
+    if bmi < 18.5:
+        print("UNDERWEIGHT")
+    if bmi >= 18.5 and bmi <= 24.9:
+        print("NORMAL")
+    if bmi >= 25 and bmi <=29.9:
+        print("OVERWEIGHT")
+    if bmi >= 30 and bmi<=34.9:
+        print("OBESE")
+    if bmi > 35:
+        print("EXTREMLY OBESE")
+
+#
 
 
 
