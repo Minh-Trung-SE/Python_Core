4a8afbd1ee64cb034e6e38b27702d9b33e5f579b
tronglvIA <trongluongvania@gmail.com>
2021-08-09 18:17:25 +0700

    9/8/2021

--- Lesson_6/all_in_one.py
+++ Lesson_6/all_in_one.py
@@ -77,6 +77,59 @@ def change_upper_lower(msg):
             new_str += msg[i]
     return new_str
 
+# 6.9 exercise
+def count_digitos_impares(n):
+    count = 1
+    if n == 0:
+        return 0
+    elif (n%10)%2 == 0:
+        return count_digitos_impares(n//10)
+    elif (n%10)%2 == 1:
+        return count + count_digitos_impares(n//10)
+
+# 6.10 exercise fibonacci
+def recur_fibo(n):
+    if n <= 0:
+        print("Incorrect input")
+        # First Fibonacci number is 0
+    elif n == 1:
+        return 0
+        # Second Fibonacci number is 1
+    elif n == 2:
+        return 1
+    else:
+        return recur_fibo(n - 1) + recur_fibo(n - 2)
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2, n):
+            c = a + b
+            a = b
+            b = c
+        return b
+# 6.11 exercise // method: getting a positive integer from the user and checking validations
+def enter_data():
+    while True:
+        n = input("Nhập 1 số nguyên: ")
+        if n.isnumeric():
+            n = int(n)
+            if n > 0:
+                print("Đã nhập số dương")
+                return n
+            print("Đã nhập số không dương. Chương trình sẽ tiếp tục!")
+        else:
+            print("Dữ liệu đã nhập không phải số nguyên")
+
+
+
 
 
 
