ede45cac0fd1b01a1468b0e386c17c2db24c63d2
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2021-08-15 22:39:35 +0700

    Select code of TrongIA

--- Lesson_8/8.7.py
+++ Lesson_8/8.7.py
@@ -1,4 +1,4 @@
-def common_member(data1,data2):
+def common_member(data1, data2):
     data1_set = set(data1)
     data2_set = set(data2)
     if len(data1_set.intersection(data2_set)) > 0:
@@ -8,5 +8,13 @@ def common_member(data1,data2):
 
 tuplex1 = (1, 2, 3, 4)
 tuplex2 = (5, 6, 7, 8, 9)
-print(common_member(tuplex1, tuplex2))
-
+# print(common_member(tuplex1, tuplex2))
+# from numpy import random
+#
+# tuple_a = tuple(random.randint(1, 40, 10))
+# tuple_b = tuple(random.randint(1, 20, 10))
+# print(tuple_b)
+# print(tuple_a)
+#
+# tuple_intersection = tuple(set(tuple_a).intersection(set(tuple_b)))
+# print(tuple_intersection)
