5b15a939457b37b71b74dc95b670fd50befa75e3
Minhtrung1772k1 <nguyenminhtrung1772kk1@gmail.com>
2021-08-16 23:25:06 +0700

    fix 8.09.py

--- Lesson_8/8.09.py
+++ Lesson_8/8.09.py
@@ -1,4 +1,5 @@
 from numpy import random
-list_number = random.uniform(1, 20, 10)
+list_number = random.randint(1, 20, 10)
 tuple_number = tuple(list_number)
 print(tuple_number)
+print(f"Sum: {sum(tuple_number)} Max: {max(tuple_number)}")
