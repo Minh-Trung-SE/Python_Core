ea22db5a587f673d80a10f5705e5d3e74977011b
tronglvIA <trongluongvania@gmail.com>
2021-08-23 19:00:58 +0700

    OOP

---
+++ Lesson_10/10.06.py
@@ -0,0 +1,23 @@
+class User:
+    first_name = ''
+    last_name = ''
+    birthday = ''
+    email = ''
+    address = ''
+    username = ''
+    password = ''
+    def __init__(self):
+        self.first_name = input("First name: ")
+        pass
+
+    def login(self):
+        pass
+
+    def logout(self):
+        pass
+
+    def update_infor(self):
+        pass
+
+
+user = User()
