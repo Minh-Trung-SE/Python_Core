d3485ad22815b5b85fcb4baa15e1e1691be4f8a5
tronglvIA <trongluongvania@gmail.com>
2021-08-23 16:52:41 +0700

    OOP

---
+++ Lesson_10/10.00.py
@@ -0,0 +1,6 @@
+class Python_OOP:
+    def __init__(self,level):
+        self.level = level
+
+james = Python_OOP('master')
+print(type(james).__name__)
---
+++ Lesson_10/10.01.py
@@ -0,0 +1,21 @@
+class Dog:
+    species = 'mammal'
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+def get_biggest_number(*args):
+    oldest_age = max(Dog.age for Dog in list_dog)
+    print(f"The oldest dog is {oldest_age} years old")
+
+
+
+list_dog = [Dog('Fake', 2),
+            Dog('Mickey', 7),
+            Dog('Fuk', 5)]
+
+get_biggest_number(*(dog for dog in list_dog))
+
+
+
+
